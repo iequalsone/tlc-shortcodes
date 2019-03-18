fecd4982928e00a81bbb7329aa2bc3f593ec2de2
Jon Howard <jon.showard@gmail.com>
2019-03-18 15:41:44 -0230

    progress

--- tlc_shortcodes.php
+++ tlc_shortcodes.php
@@ -42,19 +42,18 @@ class TLC_Shortcodes
 
 }
 
-$TLC_Shortcodes = new TLC_Shortcodes();
-
 function register_shortcodes()
 {
     add_shortcode('full-width-section', 'full_width_section');
+    add_shortcode('featured-category', 'featured_category');
 }
 
 function full_width_section($atts)
 {
-    $a = shortcode_atts(array(
+    $a = shortcode_atts([
         'page_id' => 0,
         'background_color' => '#ffffff',
-    ), $atts);
+    ], $atts);
 
     if ($a['page_id'] === 0) {
         return;
@@ -87,3 +86,81 @@ function full_width_section($atts)
 
     return $html;
 }
+
+function featured_category($atts) {
+  $a = shortcode_atts([
+    'taxonomy' => '',
+    'term' => '',
+    'background-color' => '#ffffff',
+  ], $atts);
+
+  if(empty($a['taxonomy'] || empty($a['term']))) {
+    return;
+  }
+
+  $output;
+  $term = get_term_by('slug', $a['term'], $a['taxonomy']);
+  $taxonomy = $a['taxonomy'];
+  $slug = $a['term'];
+  $name = $term->name;
+  $desc = $term->description;
+  $count = $term->count;
+
+  if($count > 0){
+    $query = new \WP_Query([
+      'post_type' => 'services',
+      'orderby' => 'menu_order',
+      'order' => 'ASC',
+      'tax_query' => [
+        [
+          'taxonomy' => $taxonomy,
+          'field'    => 'slug',
+          'terms'    => $slug,
+        ],
+      ],
+    ]);
+
+    if($query->have_posts()){
+      $output .= '<div class="featured-category row card-scroll" style="background-color: '.$a['background-color'].';"><div class="container">';
+      $output .= '<h2 class="cat-title text-center">'.$name.'</h2>';
+      $output .= '
+        <div class="row">
+          <div class="col-md-8 offset-md-2"><p class="cat-description text-center">'.$desc.'</p></div>
+        </div>';
+      $output .= '<div class="d-flex justify-content-around">';
+
+      foreach($query->posts as $p) {
+        $title = $p->post_title;
+        $excerpt = $p->post_excerpt;
+        $featured_image = get_the_post_thumbnail_url($p->ID, 'wide-thumb');
+        $permalink = get_permalink($p->ID); 
+        // $thumbnail_override = get_field('thumbnail_override', $p->ID);
+
+        if(!empty($thumbnail_override)) {
+          $image = $thumbnail_override;
+        } else {
+          $image = !empty($featured_image) ? $featured_image : "";
+        }
+
+        $output .= '
+          <div class="card shadow rounded-0 col-12 col-sm-6 col-md-4">
+            <img src="'.$image.'" class="card-img-top rounded-0" alt="'.$title.'">
+            <div class="card-body">
+              <h5 class="card-title">'.$title.'</h5>
+              <p class="card-text">'.$excerpt.'</p>
+              <a href="'.$permalink.'" class="btn btn-primary">Learn more</a>
+            </div>
+          </div>';
+      }
+      $output .= '</div></div></div>';
+    }
+
+    wp_reset_query();
+
+    return $output;
+  } else {
+    return;
+  }
+}
+
+$TLC_Shortcodes = new TLC_Shortcodes();
